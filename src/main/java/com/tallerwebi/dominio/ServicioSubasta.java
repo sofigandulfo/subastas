9349e4c822f024f2274dc067901dca00a355f822
sofigandulfo <sofigandulfo@gmail.com>
2026-04-30 15:24:45 -0300

    feat: agregar modelo, servicio y repositorio de subasta con tests

---
+++ src/main/java/com/tallerwebi/dominio/ServicioSubasta.java
@@ -0,0 +1,9 @@
+package com.tallerwebi.dominio;
+
+import com.tallerwebi.dominio.excepcion.SubastaInvalidaExeption;
+
+public interface ServicioSubasta {
+
+    void crearSubasta(Subasta subasta) throws SubastaInvalidaExeption;
+
+}
---
+++ src/test/java/com/tallerwebi/dominio/ServicioSubastaTest.java
@@ -0,0 +1,73 @@
+package com.tallerwebi.dominio;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.tallerwebi.dominio.excepcion.SubastaInvalidaExeption;
+
+public class ServicioSubastaTest {
+
+    private ServicioSubasta servicioSubasta;
+    private RepositorioSubasta repositorioSubasta;
+
+    @BeforeEach
+    public void init() {
+        repositorioSubasta = mock(RepositorioSubasta.class);
+        servicioSubasta = new ServicioSubastaImpl(repositorioSubasta);
+    }
+    
+    @Test
+    public void queAlCrearUnaSubastaValidaSeGuardaEnLaBaseDeDatosCorrectamente() throws SubastaInvalidaExeption{
+        Subasta subasta = new Subasta("Notebook", "Notebook 16gb", 1000.0, 5000.0, "Tecnologia", "nuevo");
+
+        servicioSubasta.crearSubasta(subasta);
+
+        verify(this.repositorioSubasta, times(1)).guardarSubasta(subasta);
+    }
+
+    @Test
+    public void queAlCrearUnaSubastaValidaQuedaComoActiva() throws SubastaInvalidaExeption {
+        Subasta subasta = new Subasta("Notebook", "Notebook 16gb", 1000.0, 5000.0, "Tecnologia", "nuevo");
+
+        servicioSubasta.crearSubasta(subasta);
+
+        String resultadoEsperado = "ACTIVA";
+        String resultadoObtenido = subasta.getEstadoSubasta();
+        assertEquals(resultadoEsperado, resultadoObtenido);
+    }
+
+    @Test
+    public void queNoSePuedaCrearUnaSubastaSinNombre() {
+        Subasta subasta = new Subasta("", "Notebook 16gb", 1000.0, 5000.0, "Tecnologia", "nuevo");
+
+
+        assertThrows(SubastaInvalidaExeption.class, () -> servicioSubasta.crearSubasta(subasta));
+        }
+
+    @Test
+    public void queNoSePuedaCrearUnaSubastaConPrecioInicialNegativo(){
+        Subasta subasta = new Subasta("Notebook", "Notebook 16gb", -1000.0, 5000.0, "Tecnologia", "nuevo");
+
+        assertThrows(SubastaInvalidaExeption.class, () -> servicioSubasta.crearSubasta(subasta));
+    }
+
+    @Test
+    public void queNoSePuedaCrearUnaSubastaConPrecioMaximoNegativo(){
+        Subasta subasta = new Subasta("Notebook", "Notebook 16gb", 1000.0, -5000.0, "Tecnologia", "nuevo");
+    
+        assertThrows(SubastaInvalidaExeption.class, () -> servicioSubasta.crearSubasta(subasta));
+    }
+
+    @Test
+    public void queNoSePuedaCrearUnaSubastaConPrecioMaximoMenosAlPrecioInicial(){
+        Subasta subasta = new Subasta("Notebook", "Notebook 16gb", 1000.0, 100.0, "Tecnologia", "nuevo");
+    
+        assertThrows(SubastaInvalidaExeption.class, () -> servicioSubasta.crearSubasta(subasta));
+    }
+}
