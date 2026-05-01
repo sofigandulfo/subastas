3c74b50cd134c43d26fed7d7322e46c8537e83a8
micafurlan <mariaan7448@gmail.com>
2026-05-01 18:46:35 -0300

    arregle: agregar RepositorioSubastaImpl y @Service a ServicioSubastaImpl

--- src/main/java/com/tallerwebi/dominio/ServicioSubastaImpl.java
+++ src/main/java/com/tallerwebi/dominio/ServicioSubastaImpl.java
@@ -1,11 +1,15 @@
 package com.tallerwebi.dominio;
 
 import com.tallerwebi.dominio.excepcion.SubastaInvalidaExeption;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ServicioSubastaImpl implements ServicioSubasta {
 
   private RepositorioSubasta repositorioSubasta;
 
+  @Autowired
   public ServicioSubastaImpl(RepositorioSubasta repositorioSubasta) {
     this.repositorioSubasta = repositorioSubasta;
   }
