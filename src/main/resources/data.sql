1d11e49ff45a33df22ea8fb60f1f6da044427dfe
sofigandulfo <sofigandulfo@gmail.com>
2026-05-28 10:59:27 -0300

    Agregar mas usuarios en la base de datos

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
 INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test@unlam.edu.ar', 'test', 'ADMIN', true);
 INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test2@unlam.edu.ar', 'test', 'USER', true);
+INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test3@unlam.edu.ar', 'test', 'USER', true);
+INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test4@unlam.edu.ar', 'test', 'USER', true);
