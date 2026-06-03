a4a612f6f46de4516c317c92e745477cc629157d
sofigandulfo <sofigandulfo@gmail.com>
2026-06-03 15:21:42 -0300

    Evita reinicio de base de datos al arrancar

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,15 @@
-INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test@unlam.edu.ar', 'test', 'ADMIN', true);
-INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test2@unlam.edu.ar', 'test', 'USER', true);
-INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test3@unlam.edu.ar', 'test', 'USER', true);
-INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test4@unlam.edu.ar', 'test', 'USER', true);
+INSERT INTO Usuario( email, password, rol, activo) 
+SELECT 'test@unlam.edu.ar', 'test', 'ADMIN', true
+WHERE NOT EXISTS (SELECT 1 FROM Usuario WHERE email = 'test@unlam.edu.ar');
+
+INSERT INTO Usuario(email, password, rol, activo) 
+SELECT 'test2@unlam.edu.ar', 'test', 'USER', true
+WHERE NOT EXISTS (SELECT 1 FROM Usuario WHERE email = 'test2@unlam.edu.ar');
+
+INSERT INTO Usuario(email, password, rol, activo) 
+SELECT 'test3@unlam.edu.ar', 'test', 'USER', true
+WHERE NOT EXISTS (SELECT 1 FROM Usuario WHERE email = 'test3@unlam.edu.ar');
+
+INSERT INTO Usuario(email, password, rol, activo) 
+SELECT 'test4@unlam.edu.ar', 'test', 'USER', true
+WHERE NOT EXISTS (SELECT 1 FROM Usuario WHERE email = 'test4@unlam.edu.ar');
