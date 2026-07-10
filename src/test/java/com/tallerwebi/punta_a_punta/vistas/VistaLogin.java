addf8e21136cdfa6b3671828ac0ca061930e9ec5
sofigandulfo <sofigandulfo@gmail.com>
2026-07-10 19:29:45 -0300

    test: actualizar vistas e2e

--- src/test/java/com/tallerwebi/punta_a_punta/vistas/VistaLogin.java
+++ src/test/java/com/tallerwebi/punta_a_punta/vistas/VistaLogin.java
@@ -6,7 +6,7 @@ public class VistaLogin extends VistaWeb {
 
   public VistaLogin(Page page) {
     super(page);
-    page.navigate("localhost:8080/spring/login");
+    page.navigate("localhost:8080/login");
   }
 
   public String obtenerTextoDeLaBarraDeNavegacion() {
